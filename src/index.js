5e8590908f37a347e1e7687a9c9bb6144447d8a6
verenx <155170760+verenx@users.noreply.github.com>
2024-02-04 21:49:35 +0100

    Added search engine with api calling current city and temperature value data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function refreshWeatherData(response) {
+  console.log(response);
+  let temperatureValue = document.querySelector("#weather-value");
+  let currentTemperature = Math.round(response.data.temperature.current);
+  temperatureValue.innerHTML = currentTemperature;
+  let currentCityHeading = document.querySelector("#heading-current-city");
+  currentCityHeading.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "bbb7o8acc1a3c8e9f87003f9494b2cct";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeatherData);
+}
+
 function changeCity(event) {
   event.preventDefault();
   let searchFormElement = document.querySelector("#search-input");
-  let currentCityHeading = document.querySelector("#heading-current-city");
-  currentCityHeading.innerHTML = searchFormElement.value;
+
+  searchCity(searchFormElement.value);
 }
 
 let searchFormButton = document.querySelector("#search-button");
